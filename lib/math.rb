efa55d7fe6ad69f92ba45dc8ab3baf2ee5aebfb8
Ricky dildy <rdildy92@gmail.com>
2019-09-20 22:37:48 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,34 +1,27 @@
-  num1 = "5"
-  num2 = "4"
 
 def addition(num1, num2)
+num1 = 10 
+num2 = 20
 
 
 numers = ("num1, num2")
 end
 
-num1 = "10"
-num2 = "5"
+
 def subtraction(num1, num2)
-#=> subtracts two numbers from each other
-expect(subtraction(10, 5)).to eq(5)
-numbers = ("num1, num2")
+
 end
 
-num1 = "50"
-num2 = "2"
+
+
 def division(num1, num2)
-#=> divides two numbers from each other
-expect(division(50, 2)).to eq(25)
-numbers = ("num1, num2")
+
+
 end
 
-num1 = "34"
-num2 = "5"
+
 def modulo(num1, num2)
-#=> returns the remainder of two numbers 
-expect(modulo(34, 5)).to eq(4)
-numbers = ("num1, num2")
+
 end
 
 
