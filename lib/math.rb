2d4536c861bd5d07850bed68850501425fe6721e
Ricky dildy <rdildy92@gmail.com>
2019-09-20 23:40:33 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,11 +1,17 @@
-
+num1 = 5
+num2 = 4
 def addition(num1, num2)
   #addition adds two numbers together
+puts (5 + 4).to eq(9)
 end
+end 
 
-
+num1 = 10 
+num2 = 5
 def subtraction(num1, num2)
-
+  #subtraction subtracts two numbers from each other 
+  puts (10 - 5).to eq(5)
+end
 end
 
 
