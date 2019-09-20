edf0f8c1932ec43c4b01afa10ecdaa615a9c13e6
Ricky dildy <rdildy92@gmail.com>
2019-09-20 23:55:12 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,45 @@
+num1 = 5
+num2 = 4
 def addition(num1, num2)
-
+  #addition adds two numbers together
+puts (5 + 4).to eq(9)
 end
+end 
 
+num1 = 10 
+num2 = 5
 def subtraction(num1, num2)
-
+  #subtraction subtracts two numbers from each other 
+  puts (10 - 5).to eq(5)
+end
 end
 
+num1 = 50
+num2 = 2 
 def division(num1, num2)
-
+  #division divides one number by another
+puts (50 / 2).to eq(25)
+end 
 end
 
+num1 = 4
+num2 = 30
 def multiplication(num1, num2)
+  #multiplication multiplies two numbers together
+  puts (4 * 30).to eq(120)
+end 
+end 
 
-end
-
+num1 = 34
+num2 = 5 
 def modulo(num1, num2)
-
+#modulo returns the remainder of two numbers 
+puts (34 % 5)
+end 
 end
 
+num1 = 81
 def square_root(num)
-
+#returns the square_root
+puts (81)math.sqrt 
 end
