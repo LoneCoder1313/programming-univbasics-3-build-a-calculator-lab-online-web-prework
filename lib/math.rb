6b52d83fad13d29f5a98dc0eba796a0486d40e9b
Ricky dildy <rdildy92@gmail.com>
2019-09-20 21:33:38 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,6 +4,7 @@
 def addition(num1, num2)
 #=> adds two numbers togeter
 expect(addition(5, 4)).to eq(9)
+numers = ("num1, num2")
 end
 
 num1 = "10"
@@ -11,6 +12,7 @@ num2 = "5"
 def subtraction(num1, num2)
 #=> subtracts two numbers from each other
 expect(subtraction(10, 5)).to eq(5)
+numbers = ("num1, num2")
 end
 
 num1 = "50"
@@ -18,6 +20,7 @@ num2 = "2"
 def division(num1, num2)
 #=> divides two numbers from each other
 expect(division(50, 2)).to eq(25)
+numbers = ("num1, num2")
 end
 
 num1 = "34"
@@ -25,7 +28,9 @@ num2 = "5"
 def modulo(num1, num2)
 #=> returns the remainder of two numbers 
 expect(modulo(34, 5)).to eq(4)
+numbers = ("num1, num2")
 end
+
 
 def square_root(num)
 
